f1d11e6fbd4933485d5c8aa9c16aec31864fca0e
yugiiiii740 <yugi3436@gmail.com>
2025-08-07 21:32:07 +0300

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('⏳ Komutlar yükleniyor...');
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        );
+        console.log('✅ Komutlar başarıyla yüklendi.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
